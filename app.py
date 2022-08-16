4d9f00ef8430b27685bca11470d8da7010a1eeb4
Anokye-Yaw <106571751+Anokye-Yaw@users.noreply.github.com>
2022-08-16 22:33:27 +0000

    Add files via gitbash

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from itertools import count
 from flask import Flask, render_template, request, redirect
 from models import db, StudentModel
 
@@ -13,7 +14,25 @@ def create_table():
     
 @app.route('/create', methods=['GET', 'POST'])
 def create():
-    return render_template('create.html')
+    if request.method == 'GET':
+       return render_template('create.html')
 
+    if request.method == 'POST':
+       # hobby variable stores an array
+       hobby = request.form.getlist('hobbies')
+       # hobbies variable covert hobby array into string
+       hobbies = ",".join(map(str, hobby))
+       first_name = request.form['first_name']
+       last_name = request.form['last_name']
+       email = request.form['email']
+       password = request.form['password']
+       gender = request.form['gender']
+       hobbies = hobbies
+       country = request.form['country']
+       
+       students = StudentModel(first_name=first_name, last_name=last_name, email=email, password=password, gender=gender, hobbies=hobbies,country=country)
+       db.session.add(students)
+       db.session.commit()
+       return redirect('/')
 if __name__ == "__main__":
    app.run(host='localhost', port=5000)
