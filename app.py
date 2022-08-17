297c422da9a6a38140c82536f51ad4b47d61c9f6
Anokye-Yaw <106571751+Anokye-Yaw@users.noreply.github.com>
2022-08-17 17:54:16 +0000

    Add files via gitbash

--- app.py
+++ app.py
@@ -34,5 +34,48 @@ def create():
        db.session.add(students)
        db.session.commit()
        return redirect('/')
+@app.route('/')
+def RetrieveList():
+    students = StudentModel.query.all()
+    return render_template('index.html', students=students)
+
+@app.route('/<int:id>/edit', methods = ['GET', 'POST'])
+def update(id):
+    student = StudentModel.query.filter_by(id=id).first()
+    
+    if request.method == 'POST':
+      if student:   
+            # hobby variable stores an array
+            hobby = request.form.getlist('hobbies')
+            # hobbies variable covert hobby array into string
+            hobbies = ",".join(map(str, hobby))
+            student.first_name = request.form['first_name']
+            student.last_name = request.form['last_name']
+            student.email = request.form['email']
+            student.password = request.form['password']
+            student.gender = request.form['gender']
+            student.hobbies = hobbies
+            student.country = request.form['country']
+            
+            #student = StudentModel(first_name=first_name, last_name=last_name, email=email, password=password, gender=gender, hobbies=hobbies,country=country)
+            #db.session.add(students)
+            db.session.commit()
+            return redirect('/')
+      return f"Student with id ={id} does not exist" 
+    else:
+      return render_template('update.html', student = student)
+
+@app.route('/<int:id>/delete', methods=['GET', 'POST'])
+def delete(id):
+    students = StudentModel.query.get_or_404(id)
+    if request.method == 'POST':
+       if students:    
+          db.session.delete(students)
+          db.session.commit()
+          return redirect('/')
+       abort(404)
+    return render_template('delete.html')
+    
+    
 if __name__ == "__main__":
    app.run(host='localhost', port=5000)
